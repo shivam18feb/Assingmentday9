5da081fcd3112fea5440874f5c90ad51c407d6f4
unknown <shivam18feb@gmail.com>
2022-06-20 12:39:13 +0530

    Uc1

---
+++ emp_attendence.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FULL_Working_day=8
+WAGE_PER_HR=20
+
+dailyWage=0
+
+attendance=$(( RANDOM%2 ))
+echo "Random number: $attendance"
+
+if [[ $attendance == 0 ]]
+then
+	echo "Employee is absent"
+else
+	echo "Employee is present"
+	dailyWage=$(($FULL_DAY_WORKING*$WAGE_PER_HR))
+	echo "Daily wage: $dailyWage"
+fi
