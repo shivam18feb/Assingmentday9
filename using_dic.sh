7b421dcf7926a2d58763657417563dbbee21da7a
unknown <shivam18feb@gmail.com>
2022-06-20 13:49:17 +0530

    Uc8

---
+++ using_dic.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+WAGE_PER_HR=20
+FULL_DAY_WORKING_HR=8
+HALF_DAY_WORKING_HR=4
+
+
+dailyWage=0
+total_woking_hrs=0
+days=0
+
+declare -A Wage
+
+getHours() {
+	dailyHrs=$1
+	case $dailyHrs in
+		0)
+			dailyHrs=0
+			;;
+		1)
+			dailyHrs=$HALF_DAY_WORKING_HR
+			;;
+		2)
+			dailyHrs=$FULL_DAY_WORKING_HR
+			;;
+	esac
+	echo "$dailyHrs"
+}
+
+while [[ total_working_hrs -le 100 && days -le 20 ]]
+do
+	days=$(($days+1))
+	attendance=$((RANDOM%3))
+	hrs=$(getHours $attendance)
+	dailyWage=$(( $hrs * $WAGE_PER_HR ))
+	total_woking_hrs=$(( $total_woking_hrs+$hrs ))
+	Wage["Day"$days]=$dailyWage
+done
+
+echo "Daily wage: ${Wage[@]}"
+
+for(( i=0;i<=20;i++ ))
+do
+	echo "Day$i : ${Wage["Day"$i]}"
+done
